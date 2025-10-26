17c1437dffd80ffdd2067ae998fd306825d1da47
tammoous <tammoous@gmail.com>
2025-10-26 22:01:49 +0300

    fix-03

--- EKS-Cluster/main.tf
+++ EKS-Cluster/main.tf
@@ -39,7 +39,8 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
 
   eks_managed_node_groups = {
     nodes = {
